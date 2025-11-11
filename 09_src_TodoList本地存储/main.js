84c92b229861a68962107e6cd544a0de5f7ed312
ChenJunchao <junchao.chan@outlook.com>
2025-11-11 13:32:21 +0800

    feat: 添加待办事项功能，重构应用结构，支持数据持久化

---
+++ 09_src_TodoList本地存储/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+
+//关闭Vue生产提示
+Vue.config.productionTip = false
+
+
+
+new Vue({
+    el:"#app",
+    render: h => h(App)
+})
+
+window.Vue = Vue
---
+++ src/components/MySchool.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="demo">
+    <h2 class="qwe">学校名称:{{ name }}</h2>
+    <h2>地址:{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "MyStudent",
+  data() {
+    return { 
+        msg: "欢迎",
+        name:"尚硅谷",
+        address:"北京市"
+    };
+  },
+
+
+
+};
+</script>
+
+<style lang="less" scoped>
+    .demo{
+        background-color: pink;
+        .qwe{
+            font-size: 40px;
+        }
+    }
+</style>
