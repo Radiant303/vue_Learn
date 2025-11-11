af5025fdcf0755fa4b780044569b03426323d37f
ChenJunchao <junchao.chan@outlook.com>
2025-11-11 23:49:44 +0800

    feat: 添加待办事项功能，重构组件结构，支持数据持久化和全局事件总线

---
+++ 13_src_TodoList事件总线/components/PageList.vue
@@ -0,0 +1,22 @@
+<template>
+   <ul class="todo-main">
+       <PageItem 
+            v-for="item in todos" 
+            :key="item.id"
+            :todo="item">
+        </PageItem>
+    </ul>
+</template>
+
+<script>
+import PageItem from './PageItem.vue';
+export default {
+name:"PageList",
+components:{PageItem},
+props:['todos']
+}
+</script>
+
+<style>
+
+</style>
